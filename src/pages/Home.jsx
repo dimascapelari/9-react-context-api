0d9bbba2a64976571af2e968b3c0201f176d8716
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-10-18 11:46:53 -0300

    9 React Context API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
+import { useContext } from "react";
+import { CounterContext } from "../context/CounterContext";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const { counter } = useContext(CounterContext);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>valor do contador: {counter}</p>
+    </div>
+  );
 };
